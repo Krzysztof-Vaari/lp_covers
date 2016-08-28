3199871b2d3b243b781d2411d2e509cf2d8c8442
Krzysztof-Vaari <krzysztof.vaari@gmail.com>
2016-08-28 09:18:49 +0200

    strona startowa w panelach w celu naprawienia błędu pozycjonowania paginacji

--- app/models/cover.rb
+++ app/models/cover.rb
@@ -1,5 +1,6 @@
 class Cover < ApplicationRecord
   belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ ,
+  :storage => :cloudinary
 end
